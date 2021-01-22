749d4d02f5670c4dbfb9072c95e2f9bdd3556818
Urvashi Gajjar <64082203+Urvashi98@users.noreply.github.com>
2021-01-22 18:21:17 +0530

    user model

---
+++ model/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+
+const User = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User',User);
+//model creates a table instance named User, 
+// using mongoose.Schema()
